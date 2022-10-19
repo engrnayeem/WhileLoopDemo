4b2bfd92e10602eca3f834a92e0059290c3e2e6d
unknown <engr.mohammadjannatulnayeem@gmail.com>
2022-10-19 20:46:12 +0600

    code uploaded

---
+++ src/WhileloopDemo/WhileLoopDemo.java
@@ -0,0 +1,26 @@
+
+package WhileloopDemo;
+
+/**
+ *
+ * @author nayeem
+ */
+public class WhileLoopDemo {
+    public static void main(String[]args)
+    {
+    int x=1;//initialization part
+    
+    while(x<=10)//check the condition true or false 
+    {
+    
+    System.out.println(x);
+    x++;//incrementor decrement
+    
+    
+    
+    }
+    
+    
+    }
+    
+}
